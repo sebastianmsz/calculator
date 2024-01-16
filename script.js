b5d57d1fcf39682a62a045b97d6c48fef0986614
sebastianmsz <sevas.molina2004@gmail.com>
2024-01-15 19:48:39 -0500

    Add basic math operator functions,
    operate function; add variables to store each part of the operations.

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstNum = 0;
+let operator = '';
+let secondNum = 0;
+
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, operator, num2) {
+    switch (operator){
+        case '+':
+            return sum(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
